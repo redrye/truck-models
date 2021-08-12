73b68d53fc795d9ef752b914dc73d5959592ee6e
Ryan Boudreaux <boudreauxryan@gmail.com>
2021-08-12 14:03:20 -0500

    added Provider

---
+++ src/TruckModelsProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Redrye\TruckModels;
+
+use Illuminate\Support\ServiceProvider;
+
+class WebSocketsServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../public/models/trucks/0000_00_00_000000_create_websockets_statistics_entries_table.php' => public_path('models/trucks'),
+        ], 'public');
+
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+}
